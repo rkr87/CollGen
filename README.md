ebc4faea4da46647b74c9305aed0ae3c96d2aeee
Ryan Rowland <ryan.rowland@gmail.com>
2024-08-17 16:24:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CollGen
+CollGen is a CLI based application for the **Termui Smart Pro** devices running **[CrossMix-OS](https://github.com/cizia64/CrossMix-OS)**. Its purpose to allow quick and easy dynamic generation of game collections using keyword filters on device.
+
+# Installation
+* Download the latest-release.
+* Extract the ZIP file.
+* Transfer to the **Apps** directory of your SD card.
---
+++ collgen.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PATH="/mnt/SDCARD/System/bin:$PATH"
+export LD_LIBRARY_PATH="/mnt/SDCARD/System/lib:$LD_LIBRARY_PATH"
+
+APP_DIR="$(cd "$(dirname "$0")" && pwd)"
+APP_NAME=$(basename "$APP_DIR")
+
+. "$APP_DIR/config.sh"
+. "$APP_DIR/scripts/formats.sh"
+. "$APP_DIR/scripts/utils.sh"
+. "$APP_DIR/scripts/menus.sh"
+. "$APP_DIR/scripts/collections.sh"
+. "$APP_DIR/scripts/shortcuts.sh"
+. "$APP_DIR/scripts/search.sh"
+. "$APP_DIR/scripts/bulk.sh"
+
+init() {
+    local systemLanguage=$(get_json_value "$SYSTEM_FILE" $SYSTEM_LANGUAGE_KEY)    
+    langFile="$LANG_DIR/$systemLanguage"
+    echo -e "\0033\0143"
+    show_help
+    show_main_menu
+}
+
+init
